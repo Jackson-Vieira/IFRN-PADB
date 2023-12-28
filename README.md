9949e47f7059bc215e8b3f3e951d074b7d8f50c4
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-12-28 14:09:48 -0300

    rest endpoints

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 ```
 docker run --name padb-database -e POSTGRES_PASSWORD=postgres -d postgres
 ```
+
+## Commands
+
+```
+curl -X POST -H "Content-Type: application/json" -d '{
+  "name": "Your Room Name",
+  "description": "Your Room Description",
+  "enable": true,
+  "start_date": "2023-01-01",
+  "end_date": "2023-12-31",
+  "created_at": "2023-12-28T12:00:00Z",
+  "updated_at": "2023-12-28T12:00:00Z"
+}' http://localhost:5000/room/create
+```
--- app/main.py
+++ app/main.py
@@ -1,43 +1,80 @@
 import flask
-# import psycopg2
-# from psycopg2 import connect
-# from flask import request
+import psycopg2
+
+from flask import request
+from pydantic import BaseModel, ValidationError
+from decouple import config
+
+DATABASE_URL = config("DATABASE_URL")
 
 app = flask.Flask(__name__)
 
-@app.route("/create_room")
+def connect_db():
+    return psycopg2.connect(DATABASE_URL)
+
+INSERT_ROOM_SQL = """
+  INSERT INTO Rooms (name, description, enable, start_date, end_date, created_at, updated_at)
+  VALUES (%(name)s, %(description)s, %(enable)s, %(start_date)s, %(end_date)s, %(created_at)s, %(updated_at)s)
+"""
+
+GET_ROOMS_SQL = """
+  SELECT * FROM Rooms
+"""
+
+GET_ROOM_SQL = """
+  SELECT * FROM Rooms WHERE id = %(id)s
+"""
+
+UPDATE_ROOM_SQL = """
+  UPDATE Rooms SET name = %(name)s, description = %(description)s, enable = %(enable)s, start_date = %(start_date)s, end_date = %(end_date)s, updated_at = %(updated_at)s
+  WHERE id = %(id)s
+"""
+
+DELETE_ROOM_SQL = """
+  DELETE FROM Rooms WHERE id = %(id)s
+"""
+
+class RoomBase(BaseModel):
+  name: str
+  description: str
+  enable: bool
+  start_date: str
+  end_date: str
+  created_at: str
+  updated_at: str
+
+class RoomCreate(RoomBase):
+  ...
+
+class Room(RoomBase):
+  id: int
+
+@app.route("/api/v1/rooms", methods=["POST"])
 def create_room():
-  ...
-  # params = request.args.to_dict()
-  # if (not (params.get("username") and params.get("fullname") and params.get("email") and params.get("password") and params.get("photo"))): 
-  #   return "missing params"
-
-  # with connect(**config) as conn:
-  #   with conn.cursor() as cursor:
-  #     for table in tables:
-  #       cursor.execute(table)
-  #     cursor.execute(f"insert into Users (username, fullname, email, password, photo) values ('{params['username']}', '{params['fullname']}', '{params['email']}', '{params['password']}', '{params['photo']}')")
-  #     conn.commit()
-  #     return app.redirect("/")
+  try:
+    params = request.json
+    room = RoomCreate(**params)
+    return room.model_dump(), 202
+  except ValidationError as e:
+    return e.errors(), 400
     
-@app.route("/remove_room")
-def remover_room():
-  ...
-  # id = request.args.get("id")
-  # if (not id): return "missing params"
+@app.route("/api/v1/rooms/<room_id>", methods=["DELETE"])
+def remover_room(room_id):
+  return f"Remover sala com ID {room_id}", 200
 
-  # with connect(**config) as conn:
-  #   with conn.cursor() as cursor:
-  #     cursor.execute(f"delete from Users where id={id}")
-  #     return app.redirect("/")
+@app.route("/api/v1/rooms/<room_id>", methods=["PUT"]) # CASO PRATICO: QUAL A DIFERENCA ENTRE PUT E PATCH?
+def update_room(room_id):
+  try:
+    params = request.json
+    room = RoomCreate(**params)
+    print(f"Atualizando sala com ID {room_id}")
+    print("Dados recebidos:", room)
+    return room.model_dump(), 202
+  except ValidationError as e:
+    return e.errors(), 400
 
 @app.route("/", methods=["GET"])
-def dashboad():
-  # with connect(**config) as conn:
-  #   with conn.cursor() as cursor:
-  #     cursor.execute("select * from Users")
-  #     select = cursor.fetchall()
-  # return flask.render_template("dashboard.html", len=len(select), users=(select))
+def dashboard_view():
   return flask.render_template("dashboard.html")
 
 if __name__ == "__main__":
