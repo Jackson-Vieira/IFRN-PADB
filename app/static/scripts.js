ae5fa5ba6187f5334ed3697748f8383b0899cb28
Jackson-Vieira <jacksonvieiira12@gmail.com>
2024-01-06 16:04:47 -0300

    dark style

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,74 +1,65 @@
-$(document).ready(function(){
-	// Activate tooltip
-	$('[data-bs-toggle="tooltip"]').tooltip();
-	
-	// Select/Deselect checkboxes
-	const checkbox = $('table tbody input[type="checkbox"]');
-	$("#selectAll").click(function(){
-		if(this.checked){
-			checkbox.each(function(){
-				this.checked = true;                        
-			});
-		} else{
-			checkbox.each(function(){
-				this.checked = false;                        
-			});
-		} 
-	});
+const selectAll = document.querySelector("#selectAll");
 
-	checkbox.click(function(){
-		if(!this.checked){
-			$("#selectAll").prop("checked", false);
-		}
-	});
+selectAll.addEventListener("click", () => {
+  const table_checkboxes = document.querySelectorAll(
+    'table tbody input[type="checkbox"]'
+  );
 
-	$('.edit').on('click', function () {
-        var row = $(this).closest('tr');
-        var roomId = row.find('td:eq(1)').text();
-        var roomName = row.find('td:eq(2)').text();
-        var roomDescription = row.find('td:eq(3)').text();
-        var roomEnabled = row.find('td:eq(4)').text();
-        var roomStartDate = row.find('td:eq(5)').text();
-        var roomEndDate = row.find('td:eq(6)').text();
-
-        $('#editRoomId').val(roomId);
-        $('#editRoomName').val(roomName);
-        $('#editRoomDescription').val(roomDescription);
-        $('#editRoomEnabled').val(roomEnabled);
-        $('#editRoomStartDate').val(roomStartDate);
-        $('#editRoomEndDate').val(roomEndDate);
-    });
-
-
-	$('.delete').on('click', function () {
-		// Retrieve values from the selected row
-		var row = $(this).closest('tr');
-		var roomId = row.find('td:eq(1)').text();
-		console.log(roomId);
-	});
-
-	$('#addRoomForm').on('submit', function(event){
-		event.preventDefault();
-		var formData = {
-      		name: $("#addRoomName").val(),
-      		description: $("addRoomDescription").val(),
-      		enabled: $("#addRoomEnabled").val(),
-			start_date: new Date($("#addRoomStartDate").val()),
-			end_date: new Date($("#addRoomEndDate").val()),
-    	};
-
-		console.log(formData);
-
-		$.ajax({
-		    type: "POST",
-      		url: "http://localhost:5000/api/v1/rooms",
-      		data: formData,
-      		dataType: "json",
-      		encode: true,
-    	}).done(function (data) {
-      		console.log(data);
-    	});
-
-		console.log(formData)
-	});
+  table_checkboxes.forEach((elm) => {
+    elm.checked = selectAll.checked;
+  });
 });
+
+// $(document).ready(function () {
+//   // Activate tooltip
+//   $('[data-bs-toggle="tooltip"]').tooltip();
+
+//   $(".edit").on("click", function () {
+//     var row = $(this).closest("tr");
+//     var roomId = row.find("td:eq(1)").text();
+//     var roomName = row.find("td:eq(2)").text();
+//     var roomDescription = row.find("td:eq(3)").text();
+//     var roomEnabled = row.find("td:eq(4)").text();
+//     var roomStartDate = row.find("td:eq(5)").text();
+//     var roomEndDate = row.find("td:eq(6)").text();
+
+//     $("#editRoomId").val(roomId);
+//     $("#editRoomName").val(roomName);
+//     $("#editRoomDescription").val(roomDescription);
+//     $("#editRoomEnabled").val(roomEnabled);
+//     $("#editRoomStartDate").val(roomStartDate);
+//     $("#editRoomEndDate").val(roomEndDate);
+//   });
+
+//   $(".delete").on("click", function () {
+//     // Retrieve values from the selected row
+//     var row = $(this).closest("tr");
+//     var roomId = row.find("td:eq(1)").text();
+//     console.log(roomId);
+//   });
+
+//   $("#addRoomForm").on("submit", function (event) {
+//     event.preventDefault();
+//     var formData = {
+//       name: $("#addRoomName").val(),
+//       description: $("addRoomDescription").val(),
+//       enabled: $("#addRoomEnabled").val(),
+//       start_date: new Date($("#addRoomStartDate").val()),
+//       end_date: new Date($("#addRoomEndDate").val()),
+//     };
+
+//     console.log(formData);
+
+//     $.ajax({
+//       type: "POST",
+//       url: "http://localhost:5000/api/v1/rooms",
+//       data: formData,
+//       dataType: "json",
+//       encode: true,
+//     }).done(function (data) {
+//       console.log(data);
+//     });
+
+//     console.log(formData);
+//   });
+// });
