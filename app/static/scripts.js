c81c17c6cb09990e3e25858ded22b7ecee84a023
Jackson-Vieira <jacksonvieiira12@gmail.com>
2024-01-06 17:09:33 -0300

    forms events

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,5 +1,10 @@
 const selectAll = document.querySelector("#selectAll");
 
+// forms
+const addRoomForm = document.forms["addRoomForm"];
+const editRoomForm = document.forms["editRoomForm"];
+const deleteRoomForm = document.forms["deleteRoomForm"];
+
 selectAll.addEventListener("click", () => {
   const table_checkboxes = document.querySelectorAll(
     'table tbody input[type="checkbox"]'
@@ -10,56 +15,59 @@ selectAll.addEventListener("click", () => {
   });
 });
 
-// $(document).ready(function () {
-//   // Activate tooltip
-//   $('[data-bs-toggle="tooltip"]').tooltip();
+document.querySelector(".edit").addEventListener("click", (event) => {
+  const row = event.target.closest("tr");
+  const [
+    roomId,
+    roomName,
+    roomDescription,
+    roomEnable,
+    roomStartDate,
+    roomEndDate,
+  ] = Array.from(row.querySelectorAll("td"))
+    .slice(1)
+    .map((td) => td.textContent);
 
-//   $(".edit").on("click", function () {
-//     var row = $(this).closest("tr");
-//     var roomId = row.find("td:eq(1)").text();
-//     var roomName = row.find("td:eq(2)").text();
-//     var roomDescription = row.find("td:eq(3)").text();
-//     var roomEnabled = row.find("td:eq(4)").text();
-//     var roomStartDate = row.find("td:eq(5)").text();
-//     var roomEndDate = row.find("td:eq(6)").text();
+  document.querySelector("#editRoomId").value = roomId;
+  document.querySelector("#editRoomName").value = roomName;
+  document.querySelector("#editRoomDescription").value = roomDescription;
+  document.querySelector("#editRoomEnabled").value = roomEnable === "True";
+  document.querySelector("#editRoomStartDate").value = roomStartDate;
+  document.querySelector("#editRoomEndDate").value = roomEndDate;
+});
 
-//     $("#editRoomId").val(roomId);
-//     $("#editRoomName").val(roomName);
-//     $("#editRoomDescription").val(roomDescription);
-//     $("#editRoomEnabled").val(roomEnabled);
-//     $("#editRoomStartDate").val(roomStartDate);
-//     $("#editRoomEndDate").val(roomEndDate);
-//   });
+document.querySelector(".delete").addEventListener("click", function (event) {
+  const row = this.closest("tr");
+  const roomId = row.querySelectorAll("td")[1].textContent;
+  console.log("Deleted Room ID", roomId);
+  deleteRoomForm.id.value = roomId;
+});
 
-//   $(".delete").on("click", function () {
-//     // Retrieve values from the selected row
-//     var row = $(this).closest("tr");
-//     var roomId = row.find("td:eq(1)").text();
-//     console.log(roomId);
-//   });
+function getValues(form) {
+  return {
+    id: form.id.value,
+    name: form.name.value,
+    description: form.description.value,
+    enabled: form["enabled"].value,
+    start_date: form.start_date.value,
+    room_end_data: form.end_date.value,
+  };
+}
 
-//   $("#addRoomForm").on("submit", function (event) {
-//     event.preventDefault();
-//     var formData = {
-//       name: $("#addRoomName").val(),
-//       description: $("addRoomDescription").val(),
-//       enabled: $("#addRoomEnabled").val(),
-//       start_date: new Date($("#addRoomStartDate").val()),
-//       end_date: new Date($("#addRoomEndDate").val()),
-//     };
+addRoomForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let formData = getValues(this);
+  console.log(formData);
+});
 
-//     console.log(formData);
+editRoomForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let formData = getValues(this);
+  console.log(formData);
+});
 
-//     $.ajax({
-//       type: "POST",
-//       url: "http://localhost:5000/api/v1/rooms",
-//       data: formData,
-//       dataType: "json",
-//       encode: true,
-//     }).done(function (data) {
-//       console.log(data);
-//     });
-
-//     console.log(formData);
-//   });
-// });
+deleteRoomForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const roomId = deleteRoomForm.id.value;
+  console.log("Delete room with the id:", roomId);
+});
