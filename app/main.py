3803ef6919da1b5dcb482e1db09ef1d6c2286188
Jackson-Vieira <jacksonvieiira12@gmail.com>
2024-01-07 23:52:58 -0300

    fix crud table

--- app/main.py
+++ app/main.py
@@ -6,104 +6,124 @@ from flask_cors import CORS
 from pydantic import BaseModel, ValidationError
 from decouple import config
 
-import datetime
-
 from database import ROOM_TABLE, MESSAGE_TABLE
+
+from usecases.room_usecases import RoomUseCases, RoomRepository
 
 DATABASE_URL = config("DATABASE_URL")
 
 app = flask.Flask(__name__)
 CORS(app, origins=["localhost"])
 
+
 def connect_db():
     return psycopg2.connect(DATABASE_URL)
 
-# def test_db_connection():
-#     # run script to test connection with the db 
-#     ...
 
 class RoomBase(BaseModel):
-  name: str
-  description: str
-  enable: bool
-  start_date: str
-  end_date: str
+    name: str
+    description: str
+    enable: bool
+    start_date: str
+    end_date: str
+
 
 class RoomCreate(RoomBase):
-  ...
+    ...
+
 
 class Room(RoomBase):
-  id: int
+    id: int
+
 
 @app.route("/api/v1/rooms", methods=["POST"])
 def create_room():
-  try:
-    room = RoomCreate(**request.get_json())
-    room_dict = room.model_dump()
+    try:
+        room = RoomCreate(**request.get_json())
+        room_dict = room.model_dump()
 
-    with connect_db() as conn:
-      with conn.cursor() as cur:
-        cur.execute(INSERT_ROOM_SQL, room_dict)
+        db_connection = connect_db()
 
-    return room_dict, 202
+        room_repository = RoomRepository(db_connection)
+        room_uc = RoomUseCases(room_repository)
+        room_uc.create_room(room_dict)
 
-  except ValidationError as e:
-    return e.errors(), 400
-  except Exception as e:
-    print(e)
-    return "Internal Server Error", 500
-    
+        db_connection.close()
+
+        return room_dict, 202
+
+    except ValidationError as e:
+        return e.errors(), 400
+    except Exception as e:
+        print(e)
+        return "Internal Server Error", 500
+
+
 @app.route("/api/v1/rooms/<room_id>", methods=["DELETE"])
 def remover_room(room_id):
+    try:
+        db_connection = connect_db()
 
-  # TODO: check if room exists first
-  with connect_db() as conn:
-    with conn.cursor() as cur:
-      cur.execute(DELETE_ROOM_SQL, {"id": room_id})
-      conn.commit()
+        room_repository = RoomRepository(db_connection)
+        room_uc = RoomUseCases(room_repository)
+        room_uc.delete_room(rid=room_id)
 
-  return "", 200
+        db_connection.close()
 
-@app.route("/api/v1/rooms/<room_id>", methods=["PUT"]) # CASO PRATICO: QUAL A DIFERENCA ENTRE PUT E PATCH?
-def update_room(room_id):
-  try:
-    room = RoomCreate(**request.get_json())
-    room_dict = room.model_dump()
-    room_dict["updated_at"] = datetime.datetime.now()
+    # check if room exists and return a 404 if not
 
-    # TODO: check if room exists first
-    with connect_db() as conn:
-      with conn.cursor() as cur:
-        cur.execute(UPDATE_ROOM_SQL, {"id": room_id, **room_dict})
-        conn.commit()
+    except Exception as e:
+        return "", 500
 
     return "", 200
 
-  except ValidationError as e:
-    return e.errors(), 400
-  except Exception as e:
-    print(e)
-    return e, 500
+
+@app.route("/api/v1/rooms/<room_id>", methods=["PUT"])
+def update_room(room_id):
+    try:
+        room = RoomCreate(**request.get_json())
+        room_dict = room.model_dump()
+
+        db_connection = connect_db()
+
+        room_repository = RoomRepository(db_connection)
+        room_uc = RoomUseCases(room_repository)
+        room_uc.update_room(room_id, room_dict)
+
+        db_connection.close()
+
+        return "", 200
+
+    except ValidationError as e:
+        return e.errors(), 400
+    except Exception as e:
+        print(e)
+        return e, 500
+
 
 @app.route("/", methods=["GET"])
 def dashboard_view():
+    try:
+        db_connection = connect_db()
 
-  rooms = []
-  with connect_db() as conn:
-    with conn.cursor() as cur:
-      cur.execute(GET_ROOMS_SQL)
-      rooms = cur.fetchall()
+        room_repository = RoomRepository(db_connection)
+        room_uc = RoomUseCases(room_repository)
+        rooms = room_uc.get_all_rooms()
 
-  return flask.render_template("dashboard.html", rooms=rooms)
+        db_connection.close()
+
+    except Exception as e:
+        print(e)
+        return "", 500
+
+    return flask.render_template("dashboard.html", rooms=rooms)
+
 
 if __name__ == "__main__":
-  # test_db_connection()
+    with connect_db() as conn:
+        with conn.cursor() as cur:
+            cur.execute(ROOM_TABLE)
+            cur.execute(MESSAGE_TABLE)
+            conn.commit()
 
-  with connect_db() as conn:
-    with conn.cursor() as cur:
-      cur.execute(ROOM_TABLE)
-      cur.execute(MESSAGE_TABLE)
-      conn.commit()
-  
-  # TODO: only start the server if db connection is ok
-  app.run(debug=True)
+    app.run(debug=True)
